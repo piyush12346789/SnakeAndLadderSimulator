f53f8eccabf55a1f012ab4d3d3aa675380f0509c
piyush12346789 <piyushgupta024@gmail.com>
2020-10-03 17:54:08 +0530

    [Piyush] Add: Player at initial position.

---
+++ SnakeAndLadderSimulator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SnakeAndLadderSimulator
+{
+    class Program
+    {
+        public const int STARTING_POSITION = 0;
+        public const int WINNING_POSITION = 100;
+        static void Main(string[] args)
+        {
+            int currentPosition = STARTING_POSITION;
+        }
+    }
+}
