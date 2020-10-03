3be98fba48e740b8961c5be80d1a9e322039c3f1
piyush12346789 <piyushgupta024@gmail.com>
2020-10-03 18:14:33 +0530

    [Piyush] Add: Playing with two players.

--- SnakeAndLadderSimulator/Program.cs
+++ SnakeAndLadderSimulator/Program.cs
@@ -3,60 +3,26 @@ namespace SnakeAndLadderSimulator
 {
     class Program
     {
-        public const int STARTING_POSITION = 0;
         public const int WINNING_POSITION = 100;
-        public const int NO_PLAY = 1;
-        public const int LADDER = 2;
-        public const int SNAKE = 3;
-        //Returns Output Of Die Roll
-        public static int RollTheDie()
-        {
-            Random random = new Random();
-            int numOnDie = random.Next(1, 7);
-            return numOnDie;
-        }
-        //Returns Increment Or Decrement to be Made with Current Position
-        public static int NewPosition(int numOnDie)
-        {
-            int toAdd = 0;
-            Random random = new Random();
-            int toDo = random.Next(1, 4);
-            switch (toDo)
-            {
-                case NO_PLAY:
-                    break;
-                case LADDER:
-                    toAdd = numOnDie;
-                    break;
-                case SNAKE:
-                    toAdd = -numOnDie;
-                    break;
-            }
-            return toAdd;
-        }
         static void Main(string[] args)
         {
-            int currentPosition = STARTING_POSITION, nextPosition;
-            int throws = 0;
-            while (currentPosition < WINNING_POSITION)
+            SL player1 = new SL();
+            SL player2 = new SL();
+            while (player1.currentPosition < WINNING_POSITION && player2.currentPosition < WINNING_POSITION)
             {
-                int numOnDie = RollTheDie();
-                throws++;
-                int toAdd = NewPosition(numOnDie);
-                //checking if next position will exceed 100
-                if (currentPosition + toAdd > WINNING_POSITION)
-                    nextPosition = currentPosition;
-                else
-                    nextPosition = currentPosition + toAdd;
-                //checking if player has moved below position 0
-                if (nextPosition < STARTING_POSITION)
-                    currentPosition = STARTING_POSITION;
-                else
-                    currentPosition = nextPosition;
-                Console.WriteLine("Position After Die Roll No. " + throws + " is " + currentPosition);
+                player1.PlayATurn();
+                player2.PlayATurn();
             }
-            Console.WriteLine("No. Of Total Die Rolls : " + throws);
-            Console.WriteLine("Final Position of Player : " + currentPosition);
+            if (player1.currentPosition == WINNING_POSITION)
+            {
+                Console.WriteLine("Player 1 Won");
+            }
+            else
+            {
+                Console.WriteLine("Player 2 Won");
+            }
+            Console.WriteLine("Final Position of Player 1 :" + player1.currentPosition);
+            Console.WriteLine("Final Position of Player 2 :" + player2.currentPosition);
         }
     }
 }
---
+++ SnakeAndLadderSimulator/SL.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace SnakeAndLadderSimulator
+{
+    class SL
+    {
+        public const int STARTING_POSITION = 0;
+        public const int WINNING_POSITION = 100;
+        public const int NO_PLAY = 1;
+        public const int LADDER = 2;
+        public const int SNAKE = 3;
+        public int currentPosition = STARTING_POSITION, nextPosition, throws = 0;
+        public int RollTheDie()
+        {
+            Random random = new Random();
+            int numOnDie = random.Next(1, 7);
+            return numOnDie;
+        }
+        public int NewPosition(int numOnDie)
+        {
+            int toAdd = 0;
+            Random random = new Random();
+            int toDo = random.Next(1, 4);
+            switch (toDo)
+            {
+                case NO_PLAY:
+                    break;
+                case LADDER:
+                    toAdd = numOnDie;
+                    break;
+                case SNAKE:
+                    toAdd = -numOnDie;
+                    break;
+            }
+            return toAdd;
+        }
+        public void PlayATurn()
+        {
+            int numOnDie = this.RollTheDie();
+            this.throws++;
+            int toAdd = this.NewPosition(numOnDie);
+            //checking if player got ladder, so he rolls the dice again
+            if (toAdd > 0)
+            {
+                this.PlayATurn();
+            }
+            //checking if next position will exceed 100
+            if (this.currentPosition + toAdd > WINNING_POSITION)
+            {
+                this.nextPosition = this.currentPosition;
+            }
+            else
+            {
+                this.nextPosition = this.currentPosition + toAdd;
+            }
+            //checking if player has moved below starting position
+            if (this.nextPosition < STARTING_POSITION)
+            {
+                this.currentPosition = STARTING_POSITION;
+            }
+            else
+            {
+                this.currentPosition = this.nextPosition;
+            }
+        }
+    }
+}
