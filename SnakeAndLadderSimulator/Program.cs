2383a55ceb9a7cc5af711a8482906d2dec1b1fcf
piyush12346789 <piyushgupta024@gmail.com>
2020-10-03 18:00:13 +0530

    [Piyush] Add: Die is ready to roll.

--- SnakeAndLadderSimulator/Program.cs
+++ SnakeAndLadderSimulator/Program.cs
@@ -5,9 +5,17 @@ namespace SnakeAndLadderSimulator
     {
         public const int STARTING_POSITION = 0;
         public const int WINNING_POSITION = 100;
+        //Returns Output Of Die Roll
+        public static int RollTheDie()
+        {
+            Random random = new Random();
+            int numOnDie = random.Next(1, 7);
+            return numOnDie;
+        }
         static void Main(string[] args)
         {
             int currentPosition = STARTING_POSITION;
+            int numOnDie = RollTheDie();
         }
     }
 }
